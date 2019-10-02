ff3125d321b0d3f25fc85aa796569652a974f757
Cours Python <courspython@MacBook-Pro-de-Aurelien.local>
2019-10-02 16:13:15 +0200

    MAJ intermédiaire

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from pygame.locals import *
+from itertools import product
 
 # on doit "initialiser" PyGame
 pygame.init()
@@ -8,9 +9,30 @@ pygame.init()
 # et définir la taille de la fenêtre (400x400)
 screen = pygame.display.set_mode((400, 400))
 
-while True:
-  for event in pygame.event.get(KEYDOWN):
-    if event.key == K_q:
-      # on quitte le programme lors d'un appui sur Q
-      sys.exit()
+CELL_SIZE = (20, 20)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
+def draw_cell(board_pos, color=WHITE):
+    for screen_pos in screen_positions(board_pos):
+            screen.set_at(screen_pos, color)
+
+def screen_positions(board_pos):
+    x, y = board_pos
+    cell_width, cell_height = CELL_SIZE
+    return (
+      (cell_width*x + col, cell_height*y+line)
+      for line, col
+      in product(range(cell_height),range(cell_width))
+    )
+
+while True:
+  for event in pygame.event.get():
+    if event.type == KEYDOWN:
+        if event.key == K_q:
+          pygame.display.quit()
+          pygame.quit()
+
+  screen.fill(BLACK)
+  draw_cell((2, 2), WHITE)
+  pygame.display.update()
